20930929ef77688381e5ecbc6739e72286713906
Derek McEnroe <derek.mcenroe@gmail.com>
2023-01-09 20:28:55 -0600

    create cart slice, hooked up to store

---
+++ client/features/cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+const Cart = () => {
+  return (
+    <>
+      <h1>This is the Cart</h1>
+      <h2>Hi</h2>
+    </>
+  );
+};
+
+export default Cart;
---
+++ client/features/cart/cartSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchCart = createAsyncThunk('fetchCart', async () => {
+  const { data } = await axios.get('http://localhost8080/api/cart/id');
+  return data;
+});
+
+export const addItem = createAsyncThunk('addItem', async () => {
+  const { data } = await axios.post('http://localhost8080/api/cartitem/id');
+  return data;
+});
+
+export const removeItem = createAsyncThunk('removeItem', async () => {
+  const { data } = await axios.delete('http://localhost8080/api/cartitem/id');
+  return data;
+});
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    id: null,
+    items: [],
+    total: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    //assumes db query is returning an array items in cart
+    builder.addCase(fetchCart, (state, action) => {
+      state.items = action.payload;
+    });
+    builder.addCase(addItem, (state, action) => {
+      state.items.push(action.payload);
+    });
+    //assumes each item in array is an object with properties
+    builder.addCase(removeItem, (state, action) => {
+      state.items.filter((item) => {
+        return item.id !== action.payload.id;
+      });
+    });
+  },
+});
+
+export default cartSlice.reducer;
