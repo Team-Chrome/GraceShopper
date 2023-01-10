74e75ae9fd9a16d6f58a5c31f563914ea168185c
Derek McEnroe <derek.mcenroe@gmail.com>
2023-01-09 20:59:15 -0600

    create rough draft of Cart component

--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -4,8 +4,40 @@ import { useSelector, useDispatch } from 'react-redux';
 const Cart = () => {
   return (
     <>
-      <h1>This is the Cart</h1>
-      <h2>Hi</h2>
+      <div>
+        <div>Shopping Cart</div>
+        <table>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>Coffee Picture</tr>
+            <tr className="flex-col">
+              <div>Coffee Name</div>
+              <div>Coffee Mfg</div>
+              <div>Pack Size</div>
+            </tr>
+            <tr>3 bags</tr>
+            <tr>$10.00</tr>
+            <tr>$30.00</tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="flex-col">
+        <div>Order Summary</div>
+        <div>
+          <div>Items: 3</div>
+          <div>$30.00</div>
+        </div>
+        <div>Shipping</div>
+        <div>TOTAL:$30.00</div>
+        <button>CHECKOUT</button>
+      </div>
     </>
   );
 };
