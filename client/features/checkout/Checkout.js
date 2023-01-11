48036bdf3f0763c73189f0fd5d1b0893b656ea1f
Winter Hardcastle <winter@hardcastle.io>
2023-01-11 17:40:58 -0500

    Adding credit form feature

--- client/features/checkout/Checkout.js
+++ client/features/checkout/Checkout.js
@@ -9,6 +9,10 @@ const Checkout = () => {
 
   const [cartTotal, setCartTotal] = useState(0);
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
+
   useEffect(() => {
     dispatch(fetchCart());
   }, []);
@@ -31,8 +35,59 @@ const Checkout = () => {
     <>
       <div className="p-12 font-sans">
         <div className="text-2xl mb-12">Checkout</div>
-        <div className="flex gap-12">
-          <div className="w-3/4"></div>
+        <div className="flex gap-24">
+          <div className="w-full flex-col max-w-3/4">
+            <form
+              className="w-full bg-white shadow-lg rounded px-8 pb-8 mb-4 mt-0 border"
+              onSubmit={handleSubmit}
+            >
+              <div>
+                <label
+                  className="block text-gray-700 text-lg font-bold mb-2"
+                  htmlFor="nameOnCard"
+                >
+                  <p>Name on Card</p>
+                </label>
+                <input
+                  name="nameOnCard"
+                  type="text"
+                  className="grow shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nameOnCard"
+                  placeholder="John Doe"
+                />
+              </div>
+              <div>
+                <label
+                  className="block text-gray-700 text-lg font-bold mb-2"
+                  htmlFor="cardNumber"
+                >
+                  <p>Card Number</p>
+                </label>
+                <input
+                  name="cardNumber"
+                  type="text"
+                  className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="cardNumber"
+                  placeholder="1234 1234 1234 1234"
+                />
+              </div>
+              <div>
+                <label
+                  className="block text-gray-700 text-lg font-bold mb-2"
+                  htmlFor="cardNumber"
+                >
+                  <p>Card Number</p>
+                </label>
+                <input
+                  name="cardNumber"
+                  type="text"
+                  className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="cardNumber"
+                  placeholder="1234 1234 1234 1234"
+                />
+              </div>
+            </form>
+          </div>
           <div className="flex-col items-center text-center border w-1/4 h-auto p-4">
             <div className="mb-8 text-lg font-semibold">Order Summary</div>
             <div className="flex justify-between">
