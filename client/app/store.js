9e08cc50e18d3b1e170c58909f30a550d01411d1
Winter Hardcastle <winter@hardcastle.io>
2023-01-12 14:27:11 -0500

    Edited store to include checkout slices

--- client/app/store.js
+++ client/app/store.js
@@ -3,17 +3,23 @@ import logger from "redux-logger";
 import authReducer from "../features/auth/authSlice";
 import cartReducer from "../features/cart/cartSlice";
 import singleProductReducer from "../features/singleProduct/singleProductSlice";
-import allProductsReducer from "../features/allProducts/allProductsSlice"
+import allProductsReducer from "../features/allProducts/allProductsSlice";
+import billingAddressReducer from "../features/checkout/billingSlice";
+import shippingAddressReducer from "../features/checkout/shippingSlice";
+import creditCardReducer from "../features/checkout/creditCardSlice";
 
 const store = configureStore({
   reducer: {
-    
     auth: authReducer,
-   
+
     cart: cartReducer,
     product: singleProductReducer,
- 
-    allProducts: allProductsReducer 
+
+    allProducts: allProductsReducer,
+
+    billing: billingAddressReducer,
+    shipping: shippingAddressReducer,
+    credit: creditCardReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
