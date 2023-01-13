002ab31a6d86bb7261e33862a22a9954ff06e1d6
Winter Hardcastle <winter@hardcastle.io>
2023-01-13 11:54:22 -0500

    edited index.js

--- server/api/index.js
+++ server/api/index.js
@@ -5,10 +5,10 @@ router.use("/users", require("./users"));
 router.use("/cart", require("./cart"));
 router.use("/products", require("./singleProduct"));
 router.use("/cart", require("./cart"));
-router.use("/products", require("./allProducts"))
+router.use("/products", require("./allProducts"));
 router.use("/shipping", require("./shippingAddresses"));
 router.use("/billing", require("./billingAddresses"));
-router.use("/creditCard", require("./creditCards"));)
+router.use("/creditCard", require("./creditCards"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
